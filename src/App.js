37ced0fce96ec5e8ea4dae9580168ced8555158b
Yassine Chbani <yassine.chbani@gmail.com>
2019-09-07 19:47:37 +0200

    :heavy_plus_sign: (Google Maps) Add Google Maps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps';
 import './App.css';
+
+const useGoogle = (Component: React.Component) => withScriptjs(withGoogleMap(Component));
+const MapComponent = useGoogle(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+    {props.children}
+  </GoogleMap>
+));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapComponent
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBeLMccTUfAVn3AisQ-KdFqex7rbEcnzC4&v=3.exp"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100vh` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
 }
