fbaeec70f68a3c0534d4150424cffc66cd8eac5a
Yassine Chbani <yassine.chbani@gmail.com>
2019-09-09 00:14:47 +0200

    :sparkles: (App) Basic autosuggest

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps';
+import Autosuggest from 'react-autosuggest';
 import './App.css';
 
 const useGoogle = (Component: React.Component) => withScriptjs(withGoogleMap(Component));
+
 const MapComponent = useGoogle(props => (
   <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
     {props.children}
   </GoogleMap>
 ));
 
+const getSuggestions = () => ['a', 'b'];
+
+const getSuggestionValue = value => value;
+
 function App() {
+  const [value, setValue] = React.useState('');
+  const [suggestions, setSuggestions] = React.useState([]);
+
+  const inputProps = {
+    placeholder: "Type a city's name",
+    value: value,
+    onChange: (event, { newValue }) => setValue(newValue),
+  };
+
   return (
     <div className="App">
       <MapComponent
@@ -20,7 +35,14 @@ function App() {
       />
       <div className="form-container">
         <div className="field">
-          <input type="text" name="name" />
+          <Autosuggest
+            suggestions={suggestions}
+            renderSuggestion={suggestion => <div>{suggestion}</div>}
+            getSuggestionValue={getSuggestionValue}
+            onSuggestionsFetchRequested={({ value }) => setSuggestions(getSuggestions(value))}
+            onSuggestionsClearRequested={() => setSuggestions([])}
+            inputProps={inputProps}
+          />
         </div>
       </div>
     </div>
